de97baef8191b9131b4b027bb43eddf6522f56f0
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2024-11-11 13:46:57 +0200

    Update help.js

--- help.js
+++ help.js
@@ -68,7 +68,7 @@ module.exports = {
             categorizedCommands.forEach((commands, category) => {
                 helpMessage += `📂 *${category}*\n`; // Emoji for each category
                 commands.forEach(command => {
-                    helpMessage += `🔹*${command.name}* \n`;
+                    helpMessage += ` 💠 *${command.name}* \n`;
                 });
                 helpMessage += '\n';
             });
