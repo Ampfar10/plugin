148fb86796cfbc50d665d1bce79fe31f167ce7b6
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2024-11-17 09:02:43 +0200

    Update join.js

--- join.js
+++ join.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'join',
     description: 'Joins a WhatsApp group using the provided invite link.',
-    category: 'Group Management',
+    category: '🎉Group',
     async execute(conn, chatId, args, senderId) {
         if (args.length === 0) {
             return conn.sendMessage(chatId, {
