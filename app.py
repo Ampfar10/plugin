cdc86ea8b466006a688da6e810728dfc0c19ce01
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2025-01-29 21:02:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(get_qr_code())
 
     # Run the Flask app
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host="0.0.0.0")
